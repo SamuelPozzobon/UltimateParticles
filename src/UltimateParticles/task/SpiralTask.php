6ce415218b4188f5f947fb8b7ce6ef1bf3dab922
ArchonKlaus <42522736+ArchonKlaus@users.noreply.github.com>
2018-08-29 21:31:36 +0200

    Works with MCBE 1.6

--- src/UltimateParticles/task/SpiralTask.php
+++ src/UltimateParticles/task/SpiralTask.php
@@ -1,21 +1,8 @@
 <?php
 
 /*
- * This file is a part of UltimateParticles.
- * Copyright (C) 2016 hoyinm14mc
- *
- * UltimateParticles is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * UltimateParticles is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with UltimateParticles. If not, see <http://www.gnu.org/licenses/>.
+ * This plugin has been continued by Klaus - ArchonTeam
+ * Copyright 2018 by Archon Klaus
  */
 namespace UltimateParticles\task;
 
